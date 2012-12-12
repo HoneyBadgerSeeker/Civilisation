a5d5afe90b960c550b617c24db82090ff5762d55
pilpolehobbil <pilpolehobbil@gmail.com>
2012-12-12 23:43:13 +0100

    Version 0.5d
    Ajout:
    - gestion des couleurs des joueurs pour l'initialisation pour la carte
    au niveau des unités enseignant et etudiant et directeur.  ( pas encore
    des villes )
    
    A faire:
    - gerer le keyboard pour les unités ( je suis dessus demain soir )
    - gerer la creation des villes avec fonder()
    - gerer la couleur des villes et la production des villes ( updtour )
    - ... voir mes commit précédents
    
    Remarque:
    - au niveau de cette ligne de code dans FabriqueTextureCase.cs : "
    BitmapSource desert =
    System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(Properties.Resources.desert.GetHbitmap(),
    IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()); "
    j'obtiens de temps à autre un MemoryAccessViolated.
    Pourquoi? Aucune idée, ce n'est pas une méthode que j'ai faite, c'est ce
    que nous a donné Arnaud comme méthode...

---
+++ Version actuelle/ModelingCivLib/GeneratedCode/PConstantes/CouleurJoueur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PConstantes
+{
+    public class CouleurJoueur
+    {
+        public const int ROUGE = 0;
+        public const int VERT = 1;
+        public const int BLEU = 2;
+        public const int NOIR = 3;
+    }
+}
---
+++ Version actuelle/TestUnitaireMonteur/UnitTest1_Monteur.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PJoueur;
+using PInterfaces_Fabrique_Abstraite;
+using PCivilisation;
+using PImplInfo;
+using PPartie;
+using PMonteurPartie;
+using System.Collections.Generic;
+using PConstantes;
+
+namespace TestUnitaireMonteur
+{
+    [TestClass]
+    public class UnitTest1_Monteur
+    {
+        [TestMethod]
+        public void TestMonteur()
+        {
+            CreateurPartie createur = new CreateurPartie();
+            List<Joueur> liste = new List<Joueur>();
+            Civilisation civ1 = new Info("equipe aurel");
+            Civilisation civ2 = new Info("equipe quentin");
+            Civilisation civ3 = new Info("equipe florian");
+            Civilisation civ4 = new Info("equipe nicos");
+            liste.Add(new Joueur("aurel", civ1,PConstantes.CouleurJoueur.BLEU));
+            liste.Add(new Joueur("quentin", civ2, PConstantes.CouleurJoueur.NOIR));
+            liste.Add(new Joueur("florian", civ3, PConstantes.CouleurJoueur.ROUGE));
+            liste.Add(new Joueur("nicos", civ4, PConstantes.CouleurJoueur.VERT));
+
+            foreach (Joueur j in liste)
+            {
+                System.Console.WriteLine(j._nomJoueur);
+            }
+            createur.construire(TypeCarte.PETITE, liste);
+
+            Partie p = createur.getPartie();
+            List<Joueur> liste_comp = p._liste_joueurs;
+
+            Assert.AreEqual(liste_comp, liste, "La liste des joueurs ne correspond pas !");
+            Assert.AreEqual(p._nbJoueur, 4, "Mauvais nombre de joueurs dans la carte !");
+            Assert.AreEqual(p._tour, 0, "Le tour n'est pas initialisé à 0 !");
+            /* pour tester quel joueur a été choisi en premier -> peut amener une erreur */
+            //Assert.AreEqual(p._joueurCourant, (new Joueur("aurel", civ1), "Le joueur courant n'est pas aurel !");
+
+        }
+    }
+}
+
